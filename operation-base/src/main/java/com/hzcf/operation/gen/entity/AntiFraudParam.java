2d8598fac1f27a333a2bef2af386b5eb2c149f81
zhengqinfeng <zhengqinfeng@huizhongcf.com>
2017-12-08 12:03:10 +0800

    反欺诈进件测试

---
+++ operation-base/src/main/java/com/hzcf/operation/gen/entity/AntiFraudParam.java
@@ -0,0 +1,524 @@
+package com.hzcf.operation.gen.entity;
+
+import java.util.List;
+
+/***
+ * 进件测试dto
+ * time:2017年12月7日15:25:58
+ * author:qinfeng.zheng
+ */
+public class AntiFraudParam {
+    private String name;
+    private String mobile;
+    private String idCard;
+    private String taskId;
+    private Integer applicationType;
+    private String birthday;
+    private String gender;
+    private String race;
+    private String idCardAddress;
+    private String idCardValidDate;
+    private String idCardLegality;
+    private String thresholdsIdcard;
+    private String thresholdsFace;
+    private String idAttacked;
+    private String education;
+    private String maritalStatus;
+    private String domicile;
+    private String homeAddr;
+    private String residenceLength;
+    private String industry;
+    private String companyAddr;
+    private String workingHours;
+    private String company;
+    private String position;
+    private String monthlyIncome;
+    private String payrollDay;
+    private String relativeType;
+    private String relativeName;
+    private String relativeMobile;
+    private String nonRelativeType;
+    private String nonRelativeName;
+    private String nonRelativeMobile;
+    private String loanTerm;
+    private String loanAmount;
+    private String loanUsage;
+    private String otherChannelsInfo;
+    private String bankCard;
+    private String ip;
+    private String imsi;
+    private String imei;
+    private String idfa;
+    private String mac;
+    private String osType;
+    private String osVer;
+    private String wifiMac;
+    private String wifiSsid;
+    private String wifiBssid;
+    private String nextDataBacktracking;
+    private String nextDataHistoryDays;
+    private String emayCycle;
+    private String emayPlatform;
+    private List callRecords;
+    private List callContacts;
+    private String type;
+    private String startTime;
+    private String duration;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public Integer getApplicationType() {
+        return applicationType;
+    }
+
+    public void setApplicationType(Integer applicationType) {
+        this.applicationType = applicationType;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getIdCardAddress() {
+        return idCardAddress;
+    }
+
+    public void setIdCardAddress(String idCardAddress) {
+        this.idCardAddress = idCardAddress;
+    }
+
+    public String getIdCardValidDate() {
+        return idCardValidDate;
+    }
+
+    public void setIdCardValidDate(String idCardValidDate) {
+        this.idCardValidDate = idCardValidDate;
+    }
+
+    public String getIdCardLegality() {
+        return idCardLegality;
+    }
+
+    public void setIdCardLegality(String idCardLegality) {
+        this.idCardLegality = idCardLegality;
+    }
+
+    public String getThresholdsIdcard() {
+        return thresholdsIdcard;
+    }
+
+    public void setThresholdsIdcard(String thresholdsIdcard) {
+        this.thresholdsIdcard = thresholdsIdcard;
+    }
+
+    public String getThresholdsFace() {
+        return thresholdsFace;
+    }
+
+    public void setThresholdsFace(String thresholdsFace) {
+        this.thresholdsFace = thresholdsFace;
+    }
+
+    public String getIdAttacked() {
+        return idAttacked;
+    }
+
+    public void setIdAttacked(String idAttacked) {
+        this.idAttacked = idAttacked;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public String getDomicile() {
+        return domicile;
+    }
+
+    public void setDomicile(String domicile) {
+        this.domicile = domicile;
+    }
+
+    public String getHomeAddr() {
+        return homeAddr;
+    }
+
+    public void setHomeAddr(String homeAddr) {
+        this.homeAddr = homeAddr;
+    }
+
+    public String getResidenceLength() {
+        return residenceLength;
+    }
+
+    public void setResidenceLength(String residenceLength) {
+        this.residenceLength = residenceLength;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getCompanyAddr() {
+        return companyAddr;
+    }
+
+    public void setCompanyAddr(String companyAddr) {
+        this.companyAddr = companyAddr;
+    }
+
+    public String getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(String workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(String monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public String getPayrollDay() {
+        return payrollDay;
+    }
+
+    public void setPayrollDay(String payrollDay) {
+        this.payrollDay = payrollDay;
+    }
+
+    public String getRelativeType() {
+        return relativeType;
+    }
+
+    public void setRelativeType(String relativeType) {
+        this.relativeType = relativeType;
+    }
+
+    public String getRelativeName() {
+        return relativeName;
+    }
+
+    public void setRelativeName(String relativeName) {
+        this.relativeName = relativeName;
+    }
+
+    public String getRelativeMobile() {
+        return relativeMobile;
+    }
+
+    public void setRelativeMobile(String relativeMobile) {
+        this.relativeMobile = relativeMobile;
+    }
+
+    public String getNonRelativeType() {
+        return nonRelativeType;
+    }
+
+    public void setNonRelativeType(String nonRelativeType) {
+        this.nonRelativeType = nonRelativeType;
+    }
+
+    public String getNonRelativeName() {
+        return nonRelativeName;
+    }
+
+    public void setNonRelativeName(String nonRelativeName) {
+        this.nonRelativeName = nonRelativeName;
+    }
+
+    public String getNonRelativeMobile() {
+        return nonRelativeMobile;
+    }
+
+    public void setNonRelativeMobile(String nonRelativeMobile) {
+        this.nonRelativeMobile = nonRelativeMobile;
+    }
+
+    public String getLoanTerm() {
+        return loanTerm;
+    }
+
+    public void setLoanTerm(String loanTerm) {
+        this.loanTerm = loanTerm;
+    }
+
+    public String getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(String loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public String getLoanUsage() {
+        return loanUsage;
+    }
+
+    public void setLoanUsage(String loanUsage) {
+        this.loanUsage = loanUsage;
+    }
+
+    public String getOtherChannelsInfo() {
+        return otherChannelsInfo;
+    }
+
+    public void setOtherChannelsInfo(String otherChannelsInfo) {
+        this.otherChannelsInfo = otherChannelsInfo;
+    }
+
+    public String getBankCard() {
+        return bankCard;
+    }
+
+    public void setBankCard(String bankCard) {
+        this.bankCard = bankCard;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getImsi() {
+        return imsi;
+    }
+
+    public void setImsi(String imsi) {
+        this.imsi = imsi;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public String getIdfa() {
+        return idfa;
+    }
+
+    public void setIdfa(String idfa) {
+        this.idfa = idfa;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getOsType() {
+        return osType;
+    }
+
+    public void setOsType(String osType) {
+        this.osType = osType;
+    }
+
+    public String getOsVer() {
+        return osVer;
+    }
+
+    public void setOsVer(String osVer) {
+        this.osVer = osVer;
+    }
+
+    public String getWifiMac() {
+        return wifiMac;
+    }
+
+    public void setWifiMac(String wifiMac) {
+        this.wifiMac = wifiMac;
+    }
+
+    public String getWifiSsid() {
+        return wifiSsid;
+    }
+
+    public void setWifiSsid(String wifiSsid) {
+        this.wifiSsid = wifiSsid;
+    }
+
+    public String getWifiBssid() {
+        return wifiBssid;
+    }
+
+    public void setWifiBssid(String wifiBssid) {
+        this.wifiBssid = wifiBssid;
+    }
+
+    public String getNextDataBacktracking() {
+        return nextDataBacktracking;
+    }
+
+    public void setNextDataBacktracking(String nextDataBacktracking) {
+        this.nextDataBacktracking = nextDataBacktracking;
+    }
+
+    public String getNextDataHistoryDays() {
+        return nextDataHistoryDays;
+    }
+
+    public void setNextDataHistoryDays(String nextDataHistoryDays) {
+        this.nextDataHistoryDays = nextDataHistoryDays;
+    }
+
+    public String getEmayCycle() {
+        return emayCycle;
+    }
+
+    public void setEmayCycle(String emayCycle) {
+        this.emayCycle = emayCycle;
+    }
+
+    public String getEmayPlatform() {
+        return emayPlatform;
+    }
+
+    public void setEmayPlatform(String emayPlatform) {
+        this.emayPlatform = emayPlatform;
+    }
+
+    public List getCallRecords() {
+        return callRecords;
+    }
+
+    public void setCallRecords(List callRecords) {
+        this.callRecords = callRecords;
+    }
+
+    public List getCallContacts() {
+        return callContacts;
+    }
+
+    public void setCallContacts(List callContacts) {
+        this.callContacts = callContacts;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
