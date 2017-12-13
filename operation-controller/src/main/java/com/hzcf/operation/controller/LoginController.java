64ceef7d42efec9d2476d93e2b9ac8cfd6a03bd5
zhengqinfeng <zhengqinfeng@huizhongcf.com>
2017-12-13 10:35:35 +0800

    submen

--- operation-controller/src/main/java/com/hzcf/operation/controller/LoginController.java
+++ operation-controller/src/main/java/com/hzcf/operation/controller/LoginController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.hzcf.operation.gen.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +19,6 @@ import com.hzcf.operation.base.result.ResponseCode;
 import com.hzcf.operation.base.result.Result;
 import com.hzcf.operation.base.util.BeanUtils;
 import com.hzcf.operation.base.util.StringUtils;
-import com.hzcf.operation.gen.entity.SystemMenu;
-import com.hzcf.operation.gen.entity.SystemMenuExample;
-import com.hzcf.operation.gen.entity.SystemRoleMenu;
-import com.hzcf.operation.gen.entity.SystemRoleMenuExample;
-import com.hzcf.operation.gen.entity.SystemUser;
-import com.hzcf.operation.gen.entity.SystemUserExample;
-import com.hzcf.operation.gen.entity.SystemUserRole;
-import com.hzcf.operation.gen.entity.SystemUserRoleExample;
 import com.hzcf.operation.interceptor.SessionInterceptor;
 import com.hzcf.operation.service.SystemMenuService;
 import com.hzcf.operation.service.SystemRoleMenuService;
@@ -183,7 +176,7 @@ public class LoginController {
             }
             //获取菜单名称
             SystemMenuExample menuExample = new SystemMenuExample();
-            menuExample.createCriteria().andIdIn(menuIdList);
+            menuExample.createCriteria().andIdIn(menuIdList).andParentIdIsNull();//父菜单
                     //.andDataStatusEqualTo(1);
             menuList = menuService.selectByExample(menuExample);
 
@@ -192,4 +185,86 @@ public class LoginController {
         }
         return  ret.setData(menuList);
     }
+
+
+    /***
+     * work 得到用户的菜单权限
+     * @param request
+     * @param
+     * @return
+     */
+    @ApiOperation(value="二级菜单", notes="二级菜单")
+    @RequestMapping(value="/userSubMenu")
+    public Result getUserSumMenu(HttpServletRequest request) {
+        Result ret = new Result();
+        int userId = StringUtils.strToInt(request.getParameter("userId"));
+        List<MenuDto> menuList = new ArrayList<>();
+        //参数验证
+        if (userId<1) {
+            throw  new CustomException(ResponseCode.ERROR_PARAM,"请求参数不完整或有误!");
+        }
+
+        try {
+            //用户是否存在
+            SystemUser systemUser =   systemUserService.selectByPrimarykey(userId);
+            if (systemUser==null||systemUser.getDataStatus()!=1){
+                throw  new CustomException(ResponseCode.ERROR_PARAM,"该用户不存在!");
+            }
+            //获取登录用户对应的角色 可能对应多个角色
+            SystemUserRoleExample example = new SystemUserRoleExample();
+            example.createCriteria().andUserIdEqualTo(userId).andDataStatusEqualTo(1);
+            List<SystemUserRole> userRoleList = systemUserRoleService.selectByExample(example);
+
+            if (userRoleList.size()<1){
+                throw new CustomException(ResponseCode.ERROR_PARAM,"请联系系统管理员给您分配对应角色!");
+            }
+
+            List roleIdList = new ArrayList();
+            for (SystemUserRole userRole : userRoleList) {
+                roleIdList.add(userRole.getRoleId());
+            }
+
+            //获取角色对应的菜单ID
+            SystemRoleMenuExample roleMenuExample = new SystemRoleMenuExample();
+            roleMenuExample.createCriteria().andDataStatusEqualTo(1);
+            roleMenuExample.createCriteria().andRoleIdIn(roleIdList);
+            roleMenuExample.setDistinct(true);//去重 无法根据某一字段去重 因为后面用的是in 所有对于查询出来的结果并无影响
+            List<SystemRoleMenu> roleMenuList = roleMenuService.selectByExample(roleMenuExample);
+
+            if (roleMenuList.size()<1){
+                throw new CustomException(ResponseCode.ERROR_PARAM,"请联系管理员为该角色开通对应菜单权限!");
+            }
+
+            List menuIdList  = new ArrayList();
+            for (SystemRoleMenu roleMenu:roleMenuList){
+                menuIdList.add(roleMenu.getModuleId());//菜单Id
+            }
+            //获取菜单名称
+            SystemMenuExample menuExample = new SystemMenuExample();
+            menuExample.createCriteria().andIdIn(menuIdList).andParentIdIsNull();//父菜单
+            //.andDataStatusEqualTo(1);
+            List<SystemMenu> lisMenu = menuService.selectByExample(menuExample);
+            //处理
+            if (lisMenu.size()>0){
+                for (int x=0;x<lisMenu.size();x++){
+                    MenuDto dto = new MenuDto();
+                    SystemMenuExample subMenuExample  = new SystemMenuExample();
+                    subMenuExample.createCriteria().andParentIdEqualTo(lisMenu.get(x).getId());
+                    List<SystemMenu> subMenuList = menuService.selectByExample(subMenuExample);
+                    dto.setParentMenu(lisMenu.get(x));
+                    dto.setSubMenuList(subMenuList);
+                    menuList.add(dto);
+                }
+            }
+
+        }catch (Exception e){
+            throw  new CustomException(ResponseCode.ERROR_PARAM,"系统运行错误!");
+        }
+        return  ret.setData(menuList);
+    }
+
+    //
+
+
+
 }
